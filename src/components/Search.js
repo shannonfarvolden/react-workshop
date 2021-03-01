accf1b947a20e5d3024964a6abfbeea38d894a2b
Shannon Farvolden <shannonfarvolden@hotmail.com>
2021-03-01 21:17:37 +0000

    Add search component

---
+++ src/components/Search.js
@@ -0,0 +1,18 @@
+function Search(props) {
+  const { type, setSearch } = props;
+
+  return (
+    <>
+      <label htmlFor={`search-${type}`}>Search {type}:</label>
+      <input
+        id={`search-${type}`}
+        type="text"
+        onChange={(event) => {
+          setSearch(event.target.value);
+        }}
+      ></input>
+    </>
+  );
+}
+
+export default Search;
