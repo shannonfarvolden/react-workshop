1d5ecc375aa10ae340854cd0e34ec6b0a3f68059
Shannon Farvolden <shannonfarvolden@hotmail.com>
2021-03-10 21:40:44 +0000

    Fill in additional components

--- starter-application/src/components/Locations.js
+++ starter-application/src/components/Locations.js
@@ -1,5 +1,44 @@
+import { useState, useEffect } from 'react';
+import Search from './Search';
 import './ListItems.css';
 
-function Locations() {}
+function Locations() {
+  const [locations, setLocations] = useState([]);
+  const [search, setSearch] = useState('');
+
+  useEffect(() => {
+    async function getLocations() {
+      const response = await fetch(`https://ghibliapi.herokuapp.com/locations`);
+      const data = await response.json();
+
+      setLocations(data);
+    }
+
+    getLocations();
+  }, []);
+
+  return (
+    <>
+      <Search type={'locations'} setSearch={setSearch} />
+      <div className="list-container">
+        <div className="list-card">
+          {locations
+            .filter((location) =>
+              location.name.toLowerCase().startsWith(search.toLowerCase())
+            )
+            .map((location) => (
+              <div className="item" key={location.name}>
+                <h3>{location.name}</h3>
+                <div className="details">
+                  <p>Climate: {location.climate}</p>
+                  <p>Terrain: {location.terrain}</p>
+                </div>
+              </div>
+            ))}
+        </div>
+      </div>
+    </>
+  );
+}
 
 export default Locations;
