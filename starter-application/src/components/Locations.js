13c636181e6cfab5b9971defb118566491377112
Shannon Farvolden <shannonfarvolden@hotmail.com>
2021-03-12 13:28:17 +0000

    Clear starter components

--- starter-application/src/components/Locations.js
+++ starter-application/src/components/Locations.js
@@ -1,44 +1,5 @@
-import { useState, useEffect } from 'react';
-import Search from './Search';
 import './ListItems.css';
 
-function Locations() {
-  const [locations, setLocations] = useState([]);
-  const [search, setSearch] = useState('');
-
-  useEffect(() => {
-    async function getLocations() {
-      const response = await fetch(`https://ghibliapi.herokuapp.com/locations`);
-      const data = await response.json();
-
-      setLocations(data);
-    }
-
-    getLocations();
-  }, []);
-
-  return (
-    <>
-      <Search type={'locations'} setSearch={setSearch} />
-      <div className="list-container">
-        <div className="list-card">
-          {locations
-            .filter((location) =>
-              location.name.toLowerCase().startsWith(search.toLowerCase())
-            )
-            .map((location) => (
-              <div className="item" key={location.name}>
-                <h3>{location.name}</h3>
-                <div className="details">
-                  <p>Climate: {location.climate}</p>
-                  <p>Terrain: {location.terrain}</p>
-                </div>
-              </div>
-            ))}
-        </div>
-      </div>
-    </>
-  );
-}
+function Locations() {}
 
 export default Locations;
--- starter-application/src/components/People.js
+++ starter-application/src/components/People.js
@@ -1,45 +1,5 @@
-import { useState, useEffect } from 'react';
-import Search from './Search';
 import './ListItems.css';
 
-function People() {
-  const [people, setPeople] = useState([]);
-  const [search, setSearch] = useState('');
-
-  useEffect(() => {
-    async function getPeople() {
-      const response = await fetch(`https://ghibliapi.herokuapp.com/people`);
-      const data = await response.json();
-
-      setPeople(data);
-    }
-
-    getPeople();
-  }, []);
-
-  return (
-    <>
-      <Search type={'people'} setSearch={setSearch} />
-      <div className="list-container">
-        <div className="list-card">
-          {people
-            .filter((person) =>
-              person.name.toLowerCase().startsWith(search.toLowerCase())
-            )
-            .map((person) => (
-              <div className="item" key={person.name}>
-                <h3>{person.name}</h3>
-                <div className="details">
-                  <p>Age: {person.age}</p>
-                  <p>Hair Colour: {person.eye_color}</p>
-                  <p>Eye Colour: {person.hair_color}</p>
-                </div>
-              </div>
-            ))}
-        </div>
-      </div>
-    </>
-  );
-}
+function People() {}
 
 export default People;
